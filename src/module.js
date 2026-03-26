f1bdb32ce1061707eb2c8663e21e888283938bc3
perr0112 <prtclement.ctc@gmail.com>
2026-03-26 15:51:42 +0100

    feat(module): enhance event management and container querying

--- src/module.js
+++ src/module.js
@@ -1,13 +1,32 @@
 export class Module {
     constructor() {
         this.container = document.querySelector("[data-load-container]");
+        this.ui = {};
+        this._managedEvents = [];
     }
 
-    init() {
-        console.log("Module Parent Init");
+    $(selector) {
+        return this.container.querySelector(selector);
     }
+
+    $$(selector) {
+        return this.container.querySelectorAll(selector);
+    }
+
+    addManagedEvent(element, type, handler) {
+        element.addEventListener(type, handler);
+        this._managedEvents.push({ element, type, handler });
+    }
+
+    init() {}
 
     destroy() {
-        console.log("Module Parent Destroy");
+        this._managedEvents.forEach(({ element, type, handler }) => {
+            element.removeEventListener(type, handler);
+        });
+        this._managedEvents = [];
+
+        this.ui = {};
+        this.container = null;
     }
 }
